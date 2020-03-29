cd76b95ba1ea5c67cdef2d896e646dd0e8d2e360
Bruno Gabriel da Silva <38823062+bgsilvait@users.noreply.github.com>
2020-03-29 15:11:37 +0100

    Update custom-iis.ps1

--- custom-iis.ps1
+++ custom-iis.ps1
@@ -9,13 +9,13 @@ Function is_elevated{
 
 Function iis{
     try {
-        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "IP" ; value = $env:POD_IP}"
-        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "ServiceAccount" ; value = $env:POD_SERVICE_ACCOUNT}"
-        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "Namespace" ; value = $env:POD_NAMESPACE}"
-        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "Node" ; value = $env:NODE_NAME}"
-        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "Pod" ; value = $env:POD_NAME}"
+        $ErrorActionPreference = 'SilentlyContinue'
         Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "IP" ; value = $env:POD_IP}
-        Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile/customFields" -name "." "-value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}"
+        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "ServiceAccount" ; value = $env:POD_SERVICE_ACCOUNT}
+        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "Namespace" ; value = $env:POD_NAMESPACE}
+        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "Node" ; value = $env:NODE_NAME}
+        Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "Pod" ; value = $env:POD_NAME}
+        Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile/customFields" -name "." -value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}
         }
     catch {
         Write-Error "fail to iis custom"
