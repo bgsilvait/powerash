5120d27be75727185dfa6dc9872eba29f45d7e9f
Bruno Gabriel da Silva <38823062+bgsilvait@users.noreply.github.com>
2020-03-29 14:53:36 +0100

    Update and rename custom-iis2.ps1 to custom-iis3.ps1

--- custom-iis3.ps1
+++ custom-iis3.ps1
@@ -9,7 +9,7 @@ Function is_elevated{
 
 Function iis{
     try {
-Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "IP" ; value = $env:POD_IP}
+Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name='IP';value=$env:POD_IP}
 Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile/customFields" -name "." "-value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}"
         }
     catch {
