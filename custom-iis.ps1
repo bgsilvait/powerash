8065bae6468b9cf14e5321742f5bdb0509b7320c
Bruno Gabriel da Silva <38823062+bgsilvait@users.noreply.github.com>
2020-03-29 14:50:17 +0100

    Update custom-iis.ps1

--- custom-iis.ps1
+++ custom-iis.ps1
@@ -9,11 +9,7 @@ Function is_elevated{
 
 Function iis{
     try {
-Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "IP" ; value = $env:POD_IP}"
-Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "ServiceAccount" ; value = $env:POD_SERVICE_ACCOUNT}"
-Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "Namespace" ; value = $env:POD_NAMESPACE}"
-Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "Node" ; value = $env:NODE_NAME}"
-Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." "-AtElement @{name = "Pod" ; value = $env:POD_NAME}"
+Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' -Filter "system.webServer/httpProtocol/customHeaders" -Name "." -AtElement @{name = "IP" ; value = $env:POD_IP}
 Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile/customFields" -name "." "-value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}"
         }
     catch {
